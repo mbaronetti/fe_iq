3c5122324b0ab99e1a5643a649939db440d4e816
Manuel Baronetti <manuelbaronetti@gmail.com>
2019-09-13 19:29:41 -0300

    remove dep

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import store from "./redux/store";
@@ -12,9 +12,9 @@ store.subscribe(() => console.log(store.getState()));
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter >
       <App />
-    </BrowserRouter>
+    </HashRouter >
   </Provider>,
   document.getElementById("root")
 );
